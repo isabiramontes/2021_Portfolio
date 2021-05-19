9854b7b1c08e9ee2712309a59c3caa554ce7d57d
IPbiramontes <isabiramontes@gmail.com>
2021-05-19 18:23:48 -0400

    finished artist

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,14 @@
 /* global $*/
 $(document).ready(function() {
-    $.get("navbar.html", function(data){ // imports navbar into each page
+    $.get("navbar.html", function(data) { // imports navbar into each page
         $(".bar").replaceWith(data);
     });
-    $.get("footer.html", function(data){ // imports js into each page
+    $.get("footer.html", function(data) { // imports js into each page
         $(".footer").replaceWith(data);
     });
-    $("#mainHeader").hover(function(){ // when hovering over header, gif plays
+    $("#mainHeader").hover(function() { // when hovering over header, gif plays
         $(this).css("background", "url('img/photoville.gif')");
-        }, function(){
+    }, function() {
         $(this).css("background", "url('img/cover.png')");
     });
-    $(".timeline_magnify").click(function(){ // click to enlargen timeline photos
-        document.getElementById("overlay").style.display = "block";
-        }, function(){
-        document.getElementById("overlay").style.display = "none";
-    });
 });
-
-
